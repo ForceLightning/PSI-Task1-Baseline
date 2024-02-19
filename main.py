2fb759999b25af881fd9bd6c686db97c99380766
LimSiHui <56528048+LimSiHui@users.noreply.github.com>
2024-02-19 18:01:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from torch.utils.tensorboard import SummaryWriter
 from data.prepare_data import get_dataloader
 from database.create_database import create_database
 from models.build_model import build_model
-from train import train_intent, train_traj, train_driving
-from test import validate_intent, test_intent, predict_intent, predict_traj, get_test_traj_gt, predict_driving
+from train import train_intent
+from test import validate_intent, test_intent, predict_intent
 from utils.log import RecordResults
-from utils.evaluate_results import evaluate_intent, evaluate_traj, evaluate_driving
-from utils.get_test_intent_gt import get_intent_gt, get_test_driving_gt
-
+from utils.evaluate_results import evaluate_intent
+from utils.get_test_intent_gt import get_intent_gt
+from sklearn.model_selection import ParameterGrid
+import numpy as np
+import glob
 
 def main(args):
     writer = SummaryWriter(args.checkpoint_path)
@@ -29,54 +31,37 @@ def main(args):
     model = nn.DataParallel(model)
 
     # ''' 3. Train '''
-    if args.task_name == 'ped_intent':
-        train_intent(model, optimizer, scheduler, train_loader, val_loader, args, recorder, writer)
-    elif args.task_name == 'ped_traj':
-        train_traj(model, optimizer, scheduler, train_loader, val_loader, args, recorder, writer)
-    elif args.task_name == 'driving_decision':
-        train_driving(model, optimizer, scheduler, train_loader, val_loader, args, recorder, writer)
+    train_intent(model, optimizer, scheduler, train_loader, val_loader, args, recorder, writer)
 
-    if args.task_name == 'ped_intent':
-        val_gt_file = './test_gt/val_intent_gt.json'
-        if not os.path.exists(val_gt_file):
-            get_intent_gt(val_loader, val_gt_file, args)
-        predict_intent(model, val_loader, args, dset='val')
-        evaluate_intent(val_gt_file, args.checkpoint_path + '/results/val_intent_pred', args)
-    elif args.task_name == 'ped_traj':
-        val_gt_file = './test_gt/val_traj_gt.json'
-        if not os.path.exists(val_gt_file):
-            get_test_traj_gt(model, val_loader, args, dset='val')
-        predict_traj(model, val_loader, args, dset='val')
-        score = evaluate_traj(val_gt_file, args.checkpoint_path + '/results/val_traj_pred.json', args)
-    elif args.task_name == 'driving_decision':
-        val_gt_file = './test_gt/val_driving_gt.json'
-        if not os.path.exists(val_gt_file):
-            get_test_driving_gt(model, val_loader, args, dset='val')
-        predict_driving(model, val_loader, args, dset='val')
-        score = evaluate_driving(val_gt_file, args.checkpoint_path + '/results/val_driving_pred.json', args)
-        print("Ranking score of val set: ", score)
+    val_gt_file = './test_gt/val_intent_gt.json'
+    if not os.path.exists(val_gt_file):
+        get_intent_gt(val_loader, val_gt_file, args)
+    predict_intent(model, val_loader, args, dset='val')
+    evaluate_intent(val_gt_file, args.checkpoint_path + '/results/val_intent_pred', args)
 
     # ''' 4. Test '''
-    # test_gt_file = './test_gt/test_intent_gt.json'
-    # if not os.path.exists(test_gt_file):
-    #     get_intent_gt(test_loader, test_gt_file, args)
-    # predict_intent(model, test_loader, args, dset='test')
-    # evaluate_intent(test_gt_file, args.checkpoint_path + '/results/test_intent_prediction.json', args)
+    test_gt_file = './test_gt/test_intent_gt.json'
+    if not os.path.exists(test_gt_file):
+        get_intent_gt(test_loader, test_gt_file, args)
+    predict_intent(model, test_loader, args, dset='test')
+    test_acc = evaluate_intent(test_gt_file, args.checkpoint_path + '/results/test_intent_pred', args)
 
 if __name__ == '__main__':
     # /home/scott/Work/Toyota/PSI_Competition/Dataset
     args = get_opts()
+    # Task
+    args.task_name = 'ped_intent'
 
     if args.task_name == 'ped_intent':
         args.database_file = 'intent_database_train.pkl'
         args.intent_model = True
 
-        # intent prediction
-        args.intent_num = 2  # 3 for 'major' vote; 2 for mean intent
-        args.intent_type = 'mean' # >= 0.5 --> 1 (cross); < 0.5 --> 0 (not cross)
-        args.intent_loss = ['bce']
-        args.intent_disagreement = 1  # -1: not use disagreement 1: use disagreement to reweigh samples
-        args.intent_positive_weight = 0.5  # Reweigh BCE loss of 0/1, 0.5 = count(-1) / count(1)
+    # intent prediction
+    args.intent_num = 2  # 3 for 'major' vote; 2 for mean intent
+    args.intent_type = 'mean' # >= 0.5 --> 1 (cross); < 0.5 --> 0 (not cross)
+    args.intent_loss = ['bce']
+    args.intent_disagreement = 1  # -1: not use disagreement 1: use disagreement to reweigh samples
+    args.intent_positive_weight = 0.5  # Reweigh BCE loss of 0/1, 0.5 = count(-1) / count(1)
 
     # trajectory
     if args.task_name == 'ped_traj':
@@ -84,11 +69,6 @@ if __name__ == '__main__':
         args.intent_model = False # if (or not) use intent prediction module to support trajectory prediction
         args.traj_model = True
         args.traj_loss = ['bbox_l1']
-        # 'subtract_first_frame' #here use None, so the traj bboxes output loss is based on origianl coordinates
-        # [None (paper results) | center | L2 | subtract_first_frame (good for evidential) | divide_image_size]
-    elif args.task_name == 'driving_decision':
-        args.database_file = 'driving_database_train.pkl'
-        args.driving_loss = ['cross_entropy']
 
     args.seq_overlap_rate = 0.9 # overlap rate for trian/val set
     args.test_seq_overlap_rate = 1 # overlap for test set. if == 1, means overlap is one frame, following PIE
@@ -97,72 +77,79 @@ if __name__ == '__main__':
         args.predict_length = 1 # only make one intent prediction
     elif args.task_name == 'ped_traj':
         args.predict_length = 45
-    elif args.task_name == 'driving_decision':
-        args.predict_length = 1  # only make one prediction
 
     args.max_track_size = args.observe_length + args.predict_length
     args.crop_mode = 'enlarge'
     args.normalize_bbox = None
-   
-    # Model
-    if args.task_name == 'ped_intent':
-        args.model_name = 'lstm_int_bbox'  # LSTM module, with bboxes sequence as input, to predict intent
-        args.load_image = False # only bbox sequence as input
-    elif args.task_name == 'ped_traj':
-        args.model_name = 'lstmed_traj_bbox'
-        args.load_image = False # only bbox sequence as input
-    elif args.task_name == 'driving_decision':
-        args.model_name = 'reslstm_driving_global' 
-        args.load_image = True
+    # 'subtract_first_frame' #here use None, so the traj bboxes output loss is based on origianl coordinates
+    # [None (paper results) | center | L2 | subtract_first_frame (good for evidential) | divide_image_size]
 
+    # Model
+    args.model_name = 'tcn_int_bbox'  # LSTM module, with bboxes sequence as input, to predict intent
+    args.load_image = False # only bbox sequence as input
     if args.load_image:
-        args.backbone = 'resnet' # faster-rcnn
+        args.backbone = 'resnet'
         args.freeze_backbone = False
     else:
         args.backbone = None
         args.freeze_backbone = False
 
-
     # Train
-    args.epochs = 1
-    args.batch_size = 128
-    if args.task_name == 'ped_intent':
-        args.lr = 1e-3
-        args.loss_weights = {
-            'loss_intent': 1.0,
-            'loss_traj': 0.0,
-            'loss_driving': 0.0
-        }
-    elif args.task_name == 'ped_traj':
-        args.lr = 1e-2
-        args.loss_weights = {
-            'loss_intent': 0.0,
-            'loss_traj': 1.0,
-            'loss_driving': 0.0
-        }
-    elif args.task_name == 'driving_decision':
-        args.lr = 1e-3
-        args.loss_weights = {
-            'loss_intent': 0.0,
-            'loss_traj': 0.0,
-            'loss_driving': 1.0
-        }
+    hyperparamter_list = {
+        'lr': [1e-3],
+        'batch_size': [128],
+        'epochs': [10]
+    }
+    parameter_grid = list(ParameterGrid(hyperparamter_list))
+    best_hyperparameter = None
+    best_accuracy = 0.0
 
+    # args.epochs = 1
+    # args.batch_size = 128
+    # args.lr = 1e-3
+    args.loss_weights = {
+        'loss_intent': 1.0,
+        'loss_traj': 0.0,
+        'loss_driving': 0.0
+    }
     args.val_freq = 1
     args.test_freq = 1
     args.print_freq = 10
 
-    # Record
-    now = datetime.now()
-    time_folder = now.strftime('%Y%m%d%H%M%S')
-    args.checkpoint_path = os.path.join(args.checkpoint_path, args.task_name, args.dataset, args.model_name, time_folder)
-    if not os.path.exists(args.checkpoint_path):
-        os.makedirs(args.checkpoint_path)
-    with open(os.path.join(args.checkpoint_path, 'args.txt'), 'w') as f:
-        json.dump(args.__dict__, f, indent=4)
+    for params in parameter_grid:
+        args.lr = params['lr']
+        args.batch_size = params['batch_size']
+        args.epochs = params['epochs']
 
-    result_path = os.path.join(args.checkpoint_path, 'results')
-    if not os.path.isdir(result_path):
-        os.makedirs(result_path)
+        # Record
+        now = datetime.now()
+        time_folder = now.strftime('%Y%m%d%H%M%S')
+        args.checkpoint_path = os.path.join(args.checkpoint_path, args.task_name, args.dataset, args.model_name,
+                                            time_folder)
+        if not os.path.exists(args.checkpoint_path):
+            os.makedirs(args.checkpoint_path)
+        with open(os.path.join(args.checkpoint_path, 'args.txt'), 'w') as f:
+            json.dump(args.__dict__, f, indent=4)
 
-    main(args)
+        result_path = os.path.join(args.checkpoint_path, 'results')
+        if not os.path.isdir(result_path):
+            os.makedirs(result_path)
+
+        print("Running with Parameters:", params)  # Print the current parameters
+        main(args)
+
+        performance_file = glob.glob(os.path.join(args.checkpoint_path, '**/eval_intent_results.txt'), recursive=True)
+        for file_path in performance_file:
+            with open(file_path, 'r') as f:
+                for line in f:
+                    if line.startswith('Acc:'):
+                        val_accuracy = float(line.split(':')[1].strip())
+                        if val_accuracy > best_accuracy:
+                            best_accuracy = val_accuracy
+                            best_hyperparameters = params
+                        break
+
+    # Print best hyperparameters and accuracy
+    print("Best Hyperparameters:", best_hyperparameters)
+    print("Best Validation Accuracy:", best_accuracy)
+
